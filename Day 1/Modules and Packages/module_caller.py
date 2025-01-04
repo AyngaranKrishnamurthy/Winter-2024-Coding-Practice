bc3710ebea9133933749ffaf2fb1e36d750b78ec
Ayngaran Krishnamurthy <ayngaranrk@gmail.com>
2025-01-04 18:41:49 -0500

    Python User Defined Modules

---
+++ Day 1/Modules and Packages/module_caller.py
@@ -0,0 +1,10 @@
+import modules as md
+
+print("\n\nCalling greeting function from modules.py")
+
+md.greeting("Ayngaran") #provides name value to the greeting method to print
+print("\n\n")
+print("Calling studedntdata function from modules.py")
+
+md.studentdata(00000000) #provides id value to the studentdata method to print
+print("\n\n")
---
+++ Day 1/Modules and Packages/modules.py
@@ -0,0 +1,12 @@
+def greeting(name):
+    print("Hello "+name+", welcome to the Winter Coding Session 2024")
+
+def studentdata(id):
+    dict_name={
+        "id":id,
+        "Name":"Ayngaran Krishnamurthy",
+        "Department":"Computer Science",
+        "College":"College of Sciences",
+        "University":"Old Dominion University"
+    }
+    print(dict_name)
