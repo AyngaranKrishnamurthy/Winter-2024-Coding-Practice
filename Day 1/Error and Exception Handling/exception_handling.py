2ff56cb86ae9fbb9c41b00f0917f1fd220967eec
Ayngaran Krishnamurthy <ayngaranrk@gmail.com>
2025-01-03 23:07:53 -0500

    Error Handling

---
+++ Day 1/Error and Exception Handling/exception_handling.py
@@ -0,0 +1,14 @@
+#Error message
+
+list = [1,2,3]
+
+#print(list[3])
+#To continue execution post the error. Comment the above line(Line 5) with hash "#"
+
+#Error Handling
+
+try:
+    print(list[3])
+except:
+    print("Something went wrong")
+    
