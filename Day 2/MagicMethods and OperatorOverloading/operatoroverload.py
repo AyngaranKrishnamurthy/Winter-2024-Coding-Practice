72cd66ee271431661766d0ab5f996eede245f7ba
Ayngaran Krishnamurthy <ayngaranrk@gmail.com>
2025-01-08 21:35:59 -0500

    Update operatoroverload.py

---
+++ Day 2/MagicMethods and OperatorOverloading/operatoroverload.py
@@ -0,0 +1,3 @@
+#Operator overloading allows you to define or modify the behavior of operators (like +, -, *, etc.) for custom classes. 
+#In Python, you can overload most of the built-in operators to work with your own objects by defining special magic methods 
+
