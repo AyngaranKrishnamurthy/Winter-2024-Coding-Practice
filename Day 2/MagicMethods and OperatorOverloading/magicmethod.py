fb1718bcd7d9508b32f40615f560dca65068bd07
Ayngaran Krishnamurthy <ayngaranrk@gmail.com>
2025-01-08 21:16:27 -0500

    Magic Method

--- Day 2/MagicMethods and OperatorOverloading/magicmethod.py
+++ Day 2/MagicMethods and OperatorOverloading/magicmethod.py
@@ -1,4 +1,12 @@
 #Python Magic methods are the methods starting and ending with double underscores ‘__’. 
 #They are defined by built-in classes in Python and commonly used for operator overloading.
 
- 
+class magic():
+    def __init__(self,str):
+        self.str = str
+    
+    def __repr__(self):
+        print(self.str)
+
+obj = magic("Hello")
+obj.__repr__()
